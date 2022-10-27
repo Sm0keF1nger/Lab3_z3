c7c0618ba8f5b58f1d7c1fe2960e1904e1420e38
Sm0keF1nger <kkaptelov@gmail.com>
2022-10-27 03:40:41 +0300

    first commit

---
+++ lab3_z3/lab3_z3.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    double a, x, y, z;
+    printf("x = ");
+    scanf_s("%lf", &x);
+
+    printf("y = ");
+    scanf_s("%lf", &y);
+
+    printf("z = ");
+    scanf_s("%lf", &z);
+    
+
+    if ((pow(z, x * (-1) - 2) + (1 / pow(x, 2) + 4)) != 0) {
+        a = (1 + y) * ((x + (y / pow(x, 2) + 4)) / (pow(z, x * (-1) - 2) + (1 / pow(x, 2) + 4)));
+        printf("a = (1 + y) * ((x + (y / x^2 + 4)) / z^-x-2 + (1 / x^2 + 4)))\n  result = %.2lf", a);
+    }
+    else {
+        printf("impossible to count");
+    }
+
+
+}
